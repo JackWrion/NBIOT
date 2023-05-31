e96cb146b94eb92d3c58acad23e8635dbe75821a
JackWR <jackwrion12345@gmail.com>
2023-05-31 23:33:56 +0700

    Update Error handler, clean code

--- main/uart_async_rxtxtasks_main.c
+++ main/uart_async_rxtxtasks_main.c
@@ -14,18 +14,16 @@
 #include "string.h"
 #include "driver/gpio.h"
 
-static const int RX_BUF_SIZE = 512;
-int count = -3;
-static int status_mqtt = 0;
-static char ceng_data[128] ="";
-
-
-
+#define RX_BUF_SIZE  512
 #define TXD_PIN (GPIO_NUM_17)				// TODO config UART here
 #define RXD_PIN (GPIO_NUM_16)
 #define POWER GPIO_NUM_4
 
+static int ACK=0;
+static int Err_count = 0;
 
+
+//# INIT EVERYTHING..........//////////
 // TODO init power
 static void power_init(void){
 	gpio_config_t io_conf;
@@ -38,12 +36,7 @@ static void power_init(void){
 }
 
 
-
-
-
-
-
-void init(void) {
+static void uart_init(void) {
     const uart_config_t uart_config = {
         .baud_rate = 115200,
         .data_bits = UART_DATA_8_BITS,
@@ -59,36 +52,14 @@ void init(void) {
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-int sendData(const char* logName, const char* data)
-{
-    const int len = strlen(data);
-    const int txBytes = uart_write_bytes(UART_NUM_1, data, len);
-    ESP_LOGI(logName, "Wrote %d bytes", txBytes);
-    ESP_LOGI(logName, "write %s\n",data);
-    return txBytes;
+static void led_init(void){
+	gpio_config_t io_conf;
+	io_conf.pin_bit_mask = 1<< GPIO_NUM_2;							//*** config GPIO
+	io_conf.mode = GPIO_MODE_OUTPUT;
+	io_conf.pull_up_en = 0;
+	io_conf.pull_down_en = 1;
+	io_conf.intr_type = GPIO_INTR_DISABLE;							// vo hieu hoa interrupt
+	gpio_config(&io_conf);
 }
 
 
@@ -102,9 +73,31 @@ int sendData(const char* logName, const char* data)
 
 
 
-int count2 = 2;
-char *psi,*rsrp,*rsrq,*sinr,*cellID;
 
+
+
+
+
+
+
+
+// SEND DATA via UART
+int sendData(const char* logName, const char* data)
+{
+    const int len = strlen(data);
+    const int txBytes = uart_write_bytes(UART_NUM_1, data, len);
+    ESP_LOGI(logName, "Wrote %d bytes", txBytes);
+    ESP_LOGI(logName, "Write:  %s\n",data);
+    return txBytes;
+}
+
+
+
+
+char ceng_data[128]="";
+char *psi,*rsrp,*rsrq,*sinr,*cellID;
+int count2 = 2;
+static int ceng_len = 0;
 void getDataCENG(char* rawData){
     char* data = strstr(rawData,"\"");
     //printf("%s",b);
@@ -135,7 +128,8 @@ void getDataCENG(char* rawData){
     }
     count2 = 2;
     //printf("{\"psi\": &s,\"rsrp\": &s,\"rsrq\": &s,\"sinr\": &s,\"cellID\": &s,}", psi, rsrp, rsrq, sinr, cellID);
-    sprintf(ceng_data,"{\"psi\":%s,\"rsrp\":%s,\"rsrq\":%s,\"sinr\":%s,\"cellID\":%s}\r\r\r\r\r", psi, rsrp, rsrq, sinr, cellID);
+    sprintf(ceng_data,"{\"psi\":%s,\"rsrp\":%s,\"rsrq\":%s,\"sinr\":%s,\"cellID\":%s}", psi, rsrp, rsrq, sinr, cellID);
+    ceng_len = strlen(ceng_data);
 }
 
 
@@ -144,13 +138,10 @@ void getDataCENG(char* rawData){
 
 
 
-
-
-
-
-
+static int status_mqtt = 0;
 static void mqtt_task(void *arg)
 {
+
     static const char *MQTT_TASK_TAG = "MQTT_TASK";
     esp_log_level_set(MQTT_TASK_TAG, ESP_LOG_INFO);
     while (1) {
@@ -159,7 +150,7 @@ static void mqtt_task(void *arg)
     	if (status_mqtt == 0){
     		sendData(MQTT_TASK_TAG, "AT+CNACT=0,1\r");
     		status_mqtt = 1;
-    		vTaskDelay(1000 / portTICK_PERIOD_MS);
+    		vTaskDelay(5000 / portTICK_PERIOD_MS);
     	}
 
     	// config Host and client ID
@@ -171,10 +162,13 @@ static void mqtt_task(void *arg)
     		vTaskDelay(1000 / portTICK_PERIOD_MS);
     	}
 
-    	//config Username and Password         // PWD is very IMPORTANT
+    	//config Username and Password
     	else if (status_mqtt == 2){
     		sendData(MQTT_TASK_TAG, "AT+SMCONF=\"USERNAME\",\"JACK\"\r");
-    		sendData(MQTT_TASK_TAG, "AT+SMCONF=\"PASSWORD\",\"C3ZBwHndbwMkOXIz4HmJWRs9OrddkTfU\"\r");
+    		vTaskDelay(100 / portTICK_PERIOD_MS);
+    		sendData(MQTT_TASK_TAG, "AT+SMCONF=\"PASSWORD\",\"C3ZBwHndbwMkOXIz4HmJWRs9OrddkTfU\"\r");	// PASSWORD is very IMPORTANT
+    		vTaskDelay(100 / portTICK_PERIOD_MS);
+    		sendData(MQTT_TASK_TAG, "AT+SMCONF?\r");
     		status_mqtt = 3;
     	    vTaskDelay(1000 / portTICK_PERIOD_MS);
     	}
@@ -182,10 +176,9 @@ static void mqtt_task(void *arg)
     	// CONNECTING.....
     	else if (status_mqtt == 3){
     		sendData(MQTT_TASK_TAG, "AT+SMCONN\r");
-    	    status_mqtt = 4;
     	    vTaskDelay(20000 / portTICK_PERIOD_MS);
+    	    if (ACK) status_mqtt = 4;
     	}
-
 
 
     	//Subscribe
@@ -193,30 +186,35 @@ static void mqtt_task(void *arg)
     		//sendData(TX_TASK_TAG, "at+cpowd=0\r");
     		sendData(MQTT_TASK_TAG, "AT+SMSUB=\"messages/d86dabaa-d818-4e30-b7ee-fa649f772bda/status\",0\r");
     		status_mqtt = 5;
-    		vTaskDelay(2000 / portTICK_PERIOD_MS);
+    		vTaskDelay(1000 / portTICK_PERIOD_MS);
     	}
 
 
     	//Get CENG DATA
     	else if (status_mqtt == 5){
     		sendData(MQTT_TASK_TAG, "AT+CENG?\r");		// remember delete malloc
-    		status_mqtt = 6;
     		vTaskDelay(5000 / portTICK_PERIOD_MS);
+    		if (ACK) status_mqtt = 6;
     	}
 
     	// Publishing.......
+    	// Format-------------------
+    	// AT+SMPUB="messages/d86dabaa-d818-4e30-b7ee-fa649f772bda/update",64,0,1
+    	// -------------------------
         else if (status_mqtt == 6){
-   	   		//sendData(TX_TASK_TAG, "at+cpowd=0\r");
-   	   		sendData(MQTT_TASK_TAG, "AT+SMPUB=\"messages/d86dabaa-d818-4e30-b7ee-fa649f772bda/update\",64,0,1\r");
-   	   		status_mqtt = 7;
+        	char msg[100];
+        	sprintf(msg,"AT+SMPUB=\"messages/d86dabaa-d818-4e30-b7ee-fa649f772bda/update\",%d,0,1\r",ceng_len);
+   	   		//sendData(MQTT_TASK_TAG, "AT+SMPUB=\"messages/d86dabaa-d818-4e30-b7ee-fa649f772bda/update\",64,0,0\r");
+        	sendData(MQTT_TASK_TAG, msg);
    	   		vTaskDelay(1000 / portTICK_PERIOD_MS);
+   	   		status_mqtt = 7;
    	   	}
-    	//AT+SMPUB="messages/d86dabaa-d818-4e30-b7ee-fa649f772bda/update",63,0,1
+
 
     	//add data publish.....
         else if (status_mqtt == 7){
-           	 //sendData(TX_TASK_TAG, "at+cpowd=0\r");
-        	 printf("at step 7: %s\n",ceng_data);
+        	 //strcpy(ceng_data,"{\"psi\":367,\"rsrp\":-70,\"rsrq\":-10,\"sinr\":8,\"cellID\":151089173}   ");
+        	 //printf("Publish at step 7: %s\n",ceng_data);
            	 sendData(MQTT_TASK_TAG, ceng_data);
            	 status_mqtt = 5;
            	 vTaskDelay(10000 / portTICK_PERIOD_MS);
@@ -233,23 +231,18 @@ static void mqtt_task(void *arg)
 
 
 
-
-
-
-
-
-
-
+static int count = -3;
 static void tx_task(void *arg)
 {
     static const char *TX_TASK_TAG = "TX_TASK";
-    esp_log_level_set(TX_TASK_TAG, ESP_LOG_INFO);
+    esp_log_level_set(TX_TASK_TAG, ESP_LOG_WARN);
     while (1) {
-    	printf("Step:.. %d\n",count);
+    	printf("At preparing step:.. %d\n",count);
 
     	//TODO Turn off anyway at the first state
     	if (count == -3){
     		sendData(TX_TASK_TAG, "at+cpowd=0\r");
+    		vTaskDelay(1000 / portTICK_PERIOD_MS);
     		count = -2;
     		vTaskDelay(3000 / portTICK_PERIOD_MS);
     	}
@@ -265,13 +258,12 @@ static void tx_task(void *arg)
     	else if (count == -1){
     		gpio_set_level(POWER,1);
     		count = 0;
-    		gpio_set_level(17,1);
     		vTaskDelay(10000 / portTICK_PERIOD_MS);
     	}
 
     	// Re-call turn off echo until SIM accepting UART
     	else if (count == 0){
-    		//sendData(TX_TASK_TAG, "\r\n");
+    		gpio_set_level(17,1);
     		sendData(TX_TASK_TAG, "ate0\r");
     		vTaskDelay(1000 / portTICK_PERIOD_MS);
     	}
@@ -282,19 +274,18 @@ static void tx_task(void *arg)
     	}
     	else if (count > 1 && count < 6){
     		sendData(TX_TASK_TAG, "at\r");
-    		vTaskDelay(5000 / portTICK_PERIOD_MS);
+    		vTaskDelay(2000 / portTICK_PERIOD_MS);
     	}
 
 
     	// Out of step , turn off
     	else if (count >= 6){
-    		xTaskCreate(mqtt_task, "mqtt_task", 1024*2, NULL, configMAX_PRIORITIES-2, NULL);
+    		xTaskCreate(mqtt_task, "mqtt_task", 1024*2, NULL, configMAX_PRIORITIES-1, NULL);
     		vTaskDelete(NULL);
 //    		sendData(TX_TASK_TAG, "at+cpowd=0\r");
 //    		count = -2;
 //    		vTaskDelay(5000 / portTICK_PERIOD_MS);
     	}
-
     }
 }
 
@@ -307,28 +298,59 @@ static void rx_task(void *arg)
 
     while (1) {
         const int rxBytes = uart_read_bytes(UART_NUM_1, data, RX_BUF_SIZE, 1000 / portTICK_PERIOD_MS);
+
+
+
         if (rxBytes > 0) {
             data[rxBytes] = 0;
 
             char* temp = (char*)data;
+            ACK = 1;
+
+
+            if (strstr(temp,"+CENG")){
+               //printf("At step RX 6: Get CENG...\n");
+
+               if (rxBytes < 55) {
+            	   ESP_LOG_BUFFER_HEXDUMP(RX_TASK_TAG, data, rxBytes, ESP_LOG_ERROR);
+            	   ACK = 0;
+               }
+
+               else{
+            	   ACK = 1;
+            	   strcpy(ceng_data,(char*)data);
+            	   getDataCENG(ceng_data);
+            	   count += 1;
+            	   printf("Count: %d\n",count);
+               }
+
+            }
 
             // Process ERROR , EXCEPTION
-            if (strstr(temp,"OK")) {
+            else if (strstr(temp,"OK")) {
+            	ACK = 1;
             	count += 1;
             	printf("Count: %d\n",count);
             }
+            //
+            else if (strstr(temp,"ERROR")){
 
-            //printf("data ,mqttstatus: %s....%d\n",data,status_mqtt);
-            //////
-            if (strstr(temp,"+CENG")){
-            	printf("at step rx 6:..\n");
-            	strcpy(ceng_data,(char*)data);
-            	getDataCENG(ceng_data);
+            	ACK =0;
+            	ESP_LOG_BUFFER_HEXDUMP(RX_TASK_TAG, data, rxBytes, ESP_LOG_ERROR);
             }
 
 
-            ESP_LOGI(RX_TASK_TAG, "Read %d bytes: '%s'", rxBytes, data);
-            ESP_LOG_BUFFER_HEXDUMP(RX_TASK_TAG, data, rxBytes, ESP_LOG_INFO);
+            // handler when Error too much
+            if (ACK == 0){
+            	Err_count += 1;
+            	if (Err_count == 5) esp_restart();
+            }
+            else Err_count = 0;
+
+
+
+            ESP_LOGI(RX_TASK_TAG, "Read %d bytes:...\n", rxBytes);
+            ESP_LOG_BUFFER_HEXDUMP(RX_TASK_TAG, data, rxBytes, ESP_LOG_WARN);
         }
     }
     free(data);
@@ -342,13 +364,14 @@ static void rx_task(void *arg)
 
 
 
-
-
-
-
-
-
-
+static void led_task(void * arg){
+	int toggle = 0;
+	while (1){
+		toggle = 1-toggle;
+		gpio_set_level(2,toggle);
+		vTaskDelay(1000 / portTICK_PERIOD_MS);
+	}
+}
 
 
 
@@ -356,8 +379,11 @@ static void rx_task(void *arg)
 
 void app_main(void)
 {
-    init();
+    uart_init();
     power_init();
+    led_init();
+
     xTaskCreate(rx_task, "uart_rx_task", 1024*2, NULL, configMAX_PRIORITIES, NULL);
     xTaskCreate(tx_task, "uart_tx_task", 1024*2, NULL, configMAX_PRIORITIES-1, NULL);
+    xTaskCreate(led_task, "LED_task", 1024*2, NULL, configMAX_PRIORITIES-2, NULL);
 }
